11eb2447f4b217cd6ffa1d19af428a89098c12d5
Cristian <chrisworkingonline@gmail.com>
2023-12-29 13:07:51 -0300

    estudio y comentarios 1

--- src/components/useLocalStorage/useLocalStorage.jsx
+++ src/components/useLocalStorage/useLocalStorage.jsx
@@ -1,35 +1,67 @@
 import { useEffect, useReducer, useState } from "react";
 
+/* 
+Definimos el hook useLocalStorage() que permitirá hacer persistencia de 
+datos.
+
+Se usará un reducer para el manejo del estado global en esta aplicación, 
+y se definirá en este hook.
+
+itemName: Nombre del objeto de datos guardado en el LocalStorage
+defaultValue: Información que tendrá por defecto intemName si se encuentra vacío 
+*/
 const useLocalStorage = (itemName, defaultValue) => {
 
+  // Estado inicial para el reducer 
   const initialState = {
     loading: true,
     error: false, 
     item: defaultValue,
-    sinc: false
+    sinc: false, // Para indicar que la app está intentando conectar con base de datos
   }
 
+  
+  // Defino las acciones que actualizarán el estado 
   const actionTypes = {
-    success: 'SUCCESS', 
-    error: 'ERROR',
-    save: 'SAVE',
-    sincronize: 'SINCRONIZE',
+    success: 'SUCCESS', // Conexión con DB completa
+    error: 'ERROR', // No se logró conectar con DB
+    save: 'SAVE', // Guardar tarea editada o nueva tarea
+    sincronize: 'SINCRONIZE', // Realizar intento de sincronización con DB
   }
 
+  // Defino el objeto con todos los tipos de acciones actualizadoras de estado
   const reducerObject = (state, payload) => ({
+    // SUCCESSS es la acción que se ejecuta cuando se recarga la página, en el useEffect 
+    // de la línea 67
     [actionTypes.success]: {...state, item: payload, loading: false, sinc: true},
+    // El error pasado como payload es el obtenido con el catch del try-catch
     [actionTypes.error]: {...state, error: payload },
+    // Acción para guardar un nuevo task o un task editado
     [actionTypes.save]: {...state, item: payload },
+    // Acción para mostrar la animación de "loading"
     [actionTypes.sincronize]: {...state, loading: true, sinc: false }
   })
 
+  /* 
+  El reducer recibe como parámetro el state y la acción que se quiere realizar.
+  Por la manera en que se usa y se configura el hook useReducer(), el reducer
+  debe quedar de esta manera.
+  */
   const reducer = (state, action) => {
     return reducerObject(state, action.payload)[action.type] || state
   }
 
+  // Se invoca al hook useReducer(), pasandole el reducer y el estado inicial como parámetros.
+  // Genera un array con el state y la función dispatch.
+  // El state toma el valor de initialState, por eso luego podemos destructurarlo
   const [state, dispatch] = useReducer(reducer, initialState)
 
+  // Se destructura el state
   const { item, loading, error, sinc } = state 
+
+  /* 
+  A partir de aquí se definen todos los action creators
+  */
 
   const onSuccess = (item) => {
     dispatch({type: actionTypes.success, payload: item})
@@ -47,18 +79,31 @@ const useLocalStorage = (itemName, defaultValue) => {
     dispatch({type: actionTypes.sincronize })
   }
 
+  /* 
+  Este useEffect se ejecuta siempre que se actualiza la applicación
+  y cada vez que cambia el valor del estado "sinc"
+  */
   useEffect(() => {
     try {
       setTimeout(() => {
+        // Se consulta si hay objeto de datos guardado en el LocalStorage
         const localStorageItem = localStorage.getItem(itemName);
         let parsedItem;
 
+        // Si no encuentra objeto en el LocalStorage con el nombre "itenName",
+        // se crea de cero y se le asigna el objeto pasado como parámetro, 
+        // llamado "defaultValue".
+        // Si lo encuentra, lo parsea y lo guarda en "parsedItem" para poder
+        // usarlo en los action creators.
         if (!localStorageItem) {
           localStorage.setItem(itemName, JSON.stringify(defaultValue));
           parsedItem = defaultValue;
         } else {
           parsedItem = JSON.parse(localStorageItem);
         }
+
+        // onSuccess es como un save pero que pone el "loading" en false para 
+        // no mostrar más la animación de carga
         onSuccess(parsedItem)
       }, 1000);
     } catch (err) {
@@ -66,6 +111,11 @@ const useLocalStorage = (itemName, defaultValue) => {
     }
   }, [sinc]);
 
+
+  /* 
+  saveItem() toma el objeto con el listado de tareas, la pasa a string y
+  lo guarda en el localStorage.
+  */
   const saveItem = (newItem) => {
     try {
       const stringifiedTodos = JSON.stringify(newItem);
--- src/components/useTodos/useTodos.jsx
+++ src/components/useTodos/useTodos.jsx
@@ -4,8 +4,10 @@ import { useNavigate } from "react-router-dom";
 
 const useTodos = () => {
 
+  // Se invoca useNavigate para hacer enrutamiento
   const navigate = useNavigate()
 
+  // Se invoca el useLocalStorage y se destructuran los datos y funciones que trae
   const {
     item: todos,
     saveItem: saveTodos,
@@ -14,76 +16,84 @@ const useTodos = () => {
     sincronize
   } = useLocalStorage("TODOS_V2", []);
 
+  // Se definen estados necesarios para el funcionamiento
+  // de la aplicación
   const [completedTodos, setCompletedTodos] = useState(0);
   const [totalTodos, setTotalTodos] = useState(0);
   const [valueSearch, setValueSearch] = useState("");
 
   useEffect(() => {
+    // Se obtiene la cantidad de todos que tienen la propiedad
+    // "completed" en true
     setCompletedTodos(todos.filter((todo) => !!todo.completed).length);
     setTotalTodos(todos.length);
   }, [todos]);
 
+  // Función para actualizar contantemente el valor de búsqueda del input text
   const onSearchValueChange = (e) => {
     setValueSearch(e.target.value);
-    console.log(valueSearch)
   };
 
+  // Función para filtrar el listado de todos dependiendo de lo que se escriba
+  // en el input de búsqueda
   const filter = (text) => {
-    console.log(text)
     let filteredTodos
+    // Si no hay nada escrito, muestre todos los "todos"
     if (text.length === 0 || text === null) {
       filteredTodos = todos 
       return filteredTodos
-    } else {
+    } else { // Si hay un texto de búsqueda, filtrar
       filteredTodos = todos.filter((todo) => {
         const todoText = todo.text.toLowerCase();
         const searchText = text.toLowerCase();
         return todoText.includes(searchText);
       });
     }
-    console.log('filteredTodos: ', filteredTodos)
     return filteredTodos;  
     
   }
 
-/*   if (valueSearch.length === 0) {
-    searchedTodos = todos;
-  } else {
-    searchedTodos = todos.filter((todo) => {
-      const todoText = todo.text.toLowerCase();
-      const searchText = valueSearch.toLowerCase();
-      return todoText.includes(searchText);
-    });
-  } */
-  
-
+  // Marcar como completada una tarea
+  // Recibe como parámetros el id de la tarea y la propiedad "complete".
   const onComplete = (id, complete) => {
     complete = !complete;
+    // Encuentra el todo
     let index = todos.findIndex((todo) => todo.id === id);
-    const newTodos = [...todos];    
+    const newTodos = [...todos];
+    // Cambia true/false el estado de la propiedad "completed"    
     newTodos[index].completed = !newTodos[index].completed;
+    // Guarda el nuevo arreglo de todos en el LocalStorage
     saveTodos(newTodos);
   };
 
+  // Obtener el todo a partir del id correspondiente
   const getTodo = (id) => {
     let index = todos.findIndex((todo) => todo.id === id);
     return todos[index]
   }
 
+  // Borrar el todo con el id correspondiente
   const onDelete = (id) => {
+    // Encuentra el todo
     let index = todos.findIndex((todo) => todo.id === id);
     const newTodos = [...todos];
     newTodos.splice(index, 1);
+    // Guarda el nuevo arreglo de todos en el LocalStorage
     saveTodos(newTodos);
   };
 
+  // Editar un todo
   const onEdit = (id, text) => {
+    // Encuentra el todo
     let index = todos.findIndex((todo) => todo.id === id);
     const newTodos = [...todos];
+    // Le asigna el texto del input al todo
     newTodos[index].text = text
+    // Guarda el nuevo arreglo de todos en el LocalStorage
     saveTodos(newTodos);
   }
 
+  // Agregar un nuevo todo
   const onAdd = (text) => {  
     if (text !== "") {      
       const id = newTodoId(todos);
@@ -97,6 +107,8 @@ const useTodos = () => {
     }
   };
 
+  // Función para enrutar a la página de creación de un
+  // nuevo todo
   const createButtonHandler = () => {
     setValueSearch("");
     navigate('/new')
@@ -123,12 +135,15 @@ const useTodos = () => {
     filter
   }
 
+  // Se retornan todos los estados, estados derivados y los actualizadores
   return {
     estados,
     actualizadores
   };
 };
 
+// Función para asignar un id a un nuevo todo
+// Son ids consecutivos
 const newTodoId = (todoList) => {
   if (!todoList.length){
     return 1;
